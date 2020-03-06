a74767b14e7785416c03851b7d1bcfc2b6986b5a
Luiza Pires Ribeiro Martins <lmartins@softexpert.com>
2020-03-06 15:14:44 -0300

    novo treinamemto

---
+++ Curso JavaScript/funcoesAula.js
@@ -0,0 +1,5 @@
+function ligar(){
+    var nome = "BTS\u7231"; /* caractere de amor em japonês*/
+    alert(nome); 
+    document.getElementById("lampada") src="./projeto-glass-html5/_imagens/lampada-quebrada.jpg";
+}
