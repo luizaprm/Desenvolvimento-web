31ce89f27a2dc9fd64e42c526b813a252780680f
luizaprm <61787209+luizaprm@users.noreply.github.com>
2020-03-11 08:26:29 -0300

    Add files via upload

--- Curso JavaScript/funcoesAula.js
+++ Curso JavaScript/funcoesAula.js
@@ -1,5 +1,36 @@
-function ligar(){
-    var nome = "BTS\u7231"; /* caractere de amor em japonês*/
+/*function ligar(){
+    var nome = "BTS\u7231"; /* caractere de amor em japonês
     alert(nome); 
     document.getElementById("lampada") src="./projeto-glass-html5/_imagens/lampada-quebrada.jpg";
+}*/
+
+
+/*var s = "arroz";
+
+for (var i=0;i<s.length;i++){
+    alert(s[i]);
+}
+*/
+
+
+
+window.onload = function() {
+    document.getElementById("lampada").onclick = ligar;
+    setInterval(apresentaHoras, 1000)
+}
+
+
+function ligar() {
+    document.getElementById("lampada").src = "../projeto-glass-html5/_imagens/lampada-acesa.jpg";
+    setTimeout(desligar, 3000);
+}
+
+function desligar() {
+    document.getElementById("lampada").src = "../projeto-glass-html5/_imagens/lampada-quebrada.jpg";
+}
+
+function apresentaHoras() {
+    var agora = new Date();
+    var horas = agora.getHours() + ":" + agora.getMinutes() + ":" + agora.getSeconds();
+    document.getElementById("horas").innerHTML = horas;
 }
