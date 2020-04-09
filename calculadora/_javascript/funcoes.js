1f12a2866c4a00b69532839038e93681837b1edf
luizaprm <61787209+luizaprm@users.noreply.github.com>
2020-04-09 14:53:30 -0300

    Atualização 09/04

---
+++ calculadora/_javascript/funcoes.js
@@ -0,0 +1,36 @@
+/*function gethistorico{
+    return document.getElementById("historico").innerText
+}*/
+
+var valorPrevio = "";
+var novoValor = "";
+var resultado= "";
+var valorMemoria = "";
+
+function insertNum(num){
+    document.getElementById("tela").value+=num;
+}
+
+
+
+/*
+
+function backspace{
+
+}
+
+function clear{
+
+}
+
+function cancelEntry{
+
+}
+
+function pontenciacao{
+    return
+}
+
+function porcentagem{
+    return
+}
